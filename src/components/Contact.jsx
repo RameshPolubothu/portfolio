46e7e5c201ad281077c232fb15e7ed4001abbd8d
Ramesh <210010039@gmail.com>
2024-07-15 20:23:26 +0530

    added email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
+import emailjs from 'emailjs-com';
+
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+        from_name: name,
+        email: email,
+        message: message
+    };
+
+    emailjs.send('service_siw95mo', 'template_olpfnpz', templateParams, 'KeJHLtlpi4WT8xhUe')
+        .then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Message sent successfully!');
+        }, (error) => {
+            console.log('FAILED...', error);
+            alert('Failed to send message, please try again later.');
+        });
+};
     return (
             <section id ="contacts" className="text-gray-600 bg-blue-100 body-font relative scroll-smooth">
   <div className="container px-5 py-24 mx-auto">
@@ -13,23 +37,34 @@ const Contact = () => {
         <div className="p-2 w-1/2">
           <div className="relative">
             <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            value={name}
+            onChange={(e) => setName(e.target.value)} 
+
+            />
           </div>
         </div>
         <div className="p-2 w-1/2">
           <div className="relative">
             <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
         </div>
         <div className="p-2 w-full">
           <div className="relative">
             <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
           </div>
         </div>
         <div className="p-2 w-full">
-          <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          onClick={handleSubmit} >Button</button>
         </div>
         <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
           <a className="text-indigo-500">&copy; 2024</a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <section>
             <section className="text-gray-600 bg-blue-100 body-font">
             <div className="container mx-auto flex px-5 py-15 items-center justify-center flex-col">
-                <img className="lg:w-1/6 md:w-2/6 sm:w-[0.1] mb-30 object-center rounded-3xl" alt="ramesh" src="assets/ramesh.png" />
+                <img className="w-1/2 lg:w-1/6 md:w-2/6 sm:w-4/6 mb-10 object-center rounded-3xl" alt="ramesh" src="assets/ramesh.png" />
                 <div className="text-center lg:w-2/3 w-full">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Ramesh Polubothu</h1>
                 <p className="mb-8 text-[20px] leading-relaxed">I am Polubothu Ramesh, a 4th year BTech student in the CSE department at IIT Dharwad. My passion lies in full-stack development and problem-solving, and I am eager to showcase my skills and projects on this website.</p>
